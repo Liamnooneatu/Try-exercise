f060c2fdc76f88e1282a3c9facb9f0f892a864c3
Liam <G00420041@atu.ie>
2023-11-13 09:45:29 +0000

    added a try and catch (letter's now work)

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("please enter a number between 0 - 9;");
+        System.out.println("please enter a number between 0 - 9:");
         Scanner myscan = new Scanner(System.in);
-
+       try{
         String input = myscan.nextLine();
 
         int myNum = Integer.parseInt(input);
 
-        if (myNum >= 0 && myNum <= 9) {
-            System.out.println("You entered " + myNum);
-        } else {
-            System.out.println("not a Valid number");
+
+               if (myNum >= 0 && myNum <= 9) {
+                   System.out.println("You entered " + myNum);
+               }
+               else{
+                   System.out.println("please enter a number between 0 and 9 \n");
+               }
+
+           }
+            catch(NumberFormatException e){
+            System.out.println("please enter a Valid number \n");
         }
 
     }
