ed43c4afa2b7566c6c89efc21f28fde967e5c2a9
Liam <G00420041@atu.ie>
2023-11-13 10:01:54 +0000

    gave user chances until right numbers are entered.

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -5,22 +5,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("please enter a number between 0 - 9:");
         Scanner myscan = new Scanner(System.in);
-       try{
-        String input = myscan.nextLine();
+        int x = 0;
 
-        int myNum = Integer.parseInt(input);
+        while(x < 1){
+            try {
+                String input = myscan.nextLine();
 
+                int myNum = Integer.parseInt(input);
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("You entered " + myNum);
+                    x++;
+                }
+                else {
+                    System.out.println("please enter a number between 0 and 9 \n");
+                }
 
-               if (myNum >= 0 && myNum <= 9) {
-                   System.out.println("You entered " + myNum);
-               }
-               else{
-                   System.out.println("please enter a number between 0 and 9 \n");
-               }
-
-           }
-            catch(NumberFormatException e){
-            System.out.println("please enter a Valid number \n");
+            } catch (NumberFormatException e) {
+                System.out.println("please enter a Valid number \n");
+            }
         }
 
     }
